f9779e890a64198717fe592d41e664aade75effc
shivamjai2004 <25jaiswalsd@rbunagpur.in>
2026-06-13 23:40:26 +0530

    Optiimized a little

--- react/day-03-recoil-state-management/src/App.jsx
+++ react/day-03-recoil-state-management/src/App.jsx
@@ -1,22 +1,24 @@
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil"
-import { countAtom } from "./Store/atom"
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
+import { countAtom, evenSelector } from "./Store/atom"
+import { useMemo } from "react"
 
 function App() {
 
   return (
     <>
       <div>
-         <RecoilRoot><Count /></RecoilRoot>
-      </div>   
+        <RecoilRoot><Count /></RecoilRoot>
+      </div>
     </>
   )
 }
 function Count() {
-    console.log("Is re-rendering")
+  console.log("Is re-rendering")
 
   return <>
-    <CountRenderer/>
-    <Button/>
+    <CountRenderer />
+    <Button />
+    <IsEven></IsEven>
   </>
 }
 function CountRenderer() {
@@ -26,15 +28,29 @@ function CountRenderer() {
   </div>
 }
 function Button() {
-  const [count,setCount] = useRecoilState(countAtom);
+  const setCount = useSetRecoilState(countAtom);
+  console.log("re-render")
   return (<>
     <button onClick={() => {
-      setCount(count + 1);
-    }}>Increase</button>
-    <button onClick={() => {
-      setCount(count - 1);
+      setCount(count => count - 1);
     }}>Decrease</button>
+    <button onClick={() => {
+      setCount(count => count + 1);
+    }}>Increase</button>
+
   </>)
 
+}
+
+function IsEven() {
+  let isEven = useRecoilValue(evenSelector);
+
+  // let isEven = useMemo(function () {
+  //   return value % 2 == 0;
+  // },[value])
+
+  if (isEven) {
+    return <div>It is Even</div>
+  }
 }
 export default App
--- react/day-03-recoil-state-management/src/Store/atom.jsx
+++ react/day-03-recoil-state-management/src/Store/atom.jsx
@@ -1,7 +1,16 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 
 export const countAtom = atom({
     key:"countAtom",
     default:0
+})
+
+
+export const evenSelector = selector({
+    key:"evenSelector",
+    get : (props)=>{
+        const value = props.get(countAtom);
+        return value % 2;
+    }
 })
